1579dca11d78b23f49d1315303980727286a11fc
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-04-20 03:18:31 +0000

    Create first command and run bot

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+import os
 import discord
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
-  
+  print('Logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('+hello'):
+    await message.channel.send('Hello')
+
+
+client.run(os.getenv('token'))
