6d747cdcd53dec363b1843013aaf74ee9d1823ae
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-04-21 16:12:23 +0000

    implement quiz function and allow reading from text files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import asyncio
 from random import randrange
 from keep_alive import keep_alive
 
@@ -15,21 +16,55 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+  # returns when the author of a message is the bot itself
   if message.author == client.user:
     return
-  goats = ['oshi', '86', 'love is war']
-  if message.content.startswith('*recommend'):
-    animes = [
-      'If you are into shonen, watch Demon Slayer',
-      '86 is one of the best political dramas',
-      'Love is War is the greatest romance of all time',
-      'If you love overpowered isekai, watch Overlord or Eminence in Shadow'
-    ]
-    await message.channel.send(animes[randrange(len(animes))])
 
-  for x in goats:
-    if x in message.content.lower():
-      await message.channel.send('\U0001F410')
+  # generates a quiz for a user to guess when prompted with *quiz
+  if message.content.startswith('*quiz'):
+    questions = open("quizQuestions.txt", "r")
+    questionList = questions.readlines()
+    with open("quizAnswers.txt") as answers:
+      answerList = answers.read().splitlines()
+    randomQuestionNum = randrange(len(questionList))
+    await message.channel.send(questionList[randomQuestionNum])
+
+    # checking conditions for a response
+    def check(response):
+      return response.channel == message.channel
+
+    # receiving user input
+    notCorrect = True
+    while (notCorrect):
+      try:
+        response = await client.wait_for("message", check=check, timeout=20)
+        if response.content.lower() == answerList[randomQuestionNum]:
+          await message.channel.send('Correct!')
+          notCorrect = False
+      except asyncio.TimeoutError:
+        await message.channel.send('You failed to answer in time!')
+        notCorrect = False
+
+    questions.close()
+    answers.close()
+
+  # generates a random sentence from animes.txt when prompted by *recommend
+  if message.content.startswith('*recommend'):
+    animes = open("animes.txt", "r")
+    animeList = animes.readlines()
+    await message.channel.send(animeList[randrange(len(animeList))])
+    animes.close()
+
+  # sends a goat emoji when a term in goats.txt is mentioned
+  with open("goats.txt") as goats:
+    goatList = goats.read().splitlines()
+  words = message.content.split()
+  for x in goatList:
+    for y in words:
+      if x == y.lower():
+        await message.channel.send('\U0001F410')
+        return
+  goats.close()
 
 
 keep_alive()
