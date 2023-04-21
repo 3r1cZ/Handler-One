e41408175fed5b26114ca5d4909d005ac46a71bd
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-04-21 03:18:07 +0000

    update main with anime recommendations and implement code to keep bot running

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import discord
+from random import randrange
+from keep_alive import keep_alive
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -15,9 +17,20 @@ async def on_ready():
 async def on_message(message):
   if message.author == client.user:
     return
+  goats = ['oshi', '86', 'love is war']
+  if message.content.startswith('*recommend'):
+    animes = [
+      'If you are into shonen, watch Demon Slayer',
+      '86 is one of the best political dramas',
+      'Love is War is the greatest romance of all time',
+      'If you love overpowered isekai, watch Overlord or Eminence in Shadow'
+    ]
+    await message.channel.send(animes[randrange(len(animes))])
 
-  if message.content.startswith('+hello'):
-    await message.channel.send('Hello')
+  for x in goats:
+    if x in message.content.lower():
+      await message.channel.send('\U0001F410')
 
 
+keep_alive()
 client.run(os.getenv('token'))
