6df8a6a3308e9d3a659af4b9a7f957ac41e30f1f
3r1cZ <114354109+3r1cZ@users.noreply.github.com>
2023-04-21 18:57:13 +0000

    implement exiting and passing questions on quiz + add questions to quiz

--- main.py
+++ main.py
@@ -20,33 +20,43 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  # generates a quiz for a user to guess when prompted with *quiz
+  # generates a quiz for a user to guess when prompted by *quiz
   if message.content.startswith('*quiz'):
-    questions = open("quizQuestions.txt", "r")
-    questionList = questions.readlines()
-    with open("quizAnswers.txt") as answers:
-      answerList = answers.read().splitlines()
-    randomQuestionNum = randrange(len(questionList))
-    await message.channel.send(questionList[randomQuestionNum])
+    nextQuestion = True
+    while nextQuestion:
+      nextQuestion = False
+      questions = open("quizQuestions.txt", "r")
+      questionList = questions.readlines()
+      with open("quizAnswers.txt") as answers:
+        answerList = answers.read().splitlines()
+      randomQuestionNum = randrange(len(questionList))
+      await message.channel.send(questionList[randomQuestionNum])
 
-    # checking conditions for a response
-    def check(response):
-      return response.channel == message.channel
+      # checking conditions for a response
+      def check(response):
+        return response.channel == message.channel
 
-    # receiving user input
-    notCorrect = True
-    while (notCorrect):
-      try:
-        response = await client.wait_for("message", check=check, timeout=20)
-        if response.content.lower() == answerList[randomQuestionNum]:
-          await message.channel.send('Correct!')
+      # receiving user input
+      notCorrect = True
+      while (notCorrect):
+        try:
+          response = await client.wait_for("message", check=check, timeout=20)
+          if response.content.lower() == '*exit':
+            await message.channel.send('Exiting quiz.')
+            return
+          elif response.content.lower() == '*pass':
+            await message.channel.send('Question passed. Next Question:')
+            notCorrect = False
+            nextQuestion = True
+          elif response.content.lower() == answerList[randomQuestionNum]:
+            await message.channel.send('Correct!')
+            notCorrect = False
+        except asyncio.TimeoutError:
+          await message.channel.send('You failed to answer in time!')
           notCorrect = False
-      except asyncio.TimeoutError:
-        await message.channel.send('You failed to answer in time!')
-        notCorrect = False
 
-    questions.close()
-    answers.close()
+      questions.close()
+      answers.close()
 
   # generates a random sentence from animes.txt when prompted by *recommend
   if message.content.startswith('*recommend'):
