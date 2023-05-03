c412b682c0bb60d6b3333fc2054d355ef8546d76
3r1cZ <ericzhoubusiness0@gmail.com>
2023-05-03 14:55:20 -0400

    implement music command
    The command finishes running successfully and returns code 1 but does not actually play music.

--- music.py
+++ music.py
@@ -1,17 +1,10 @@
 import discord
-import asyncio
-import youtube_dl
-import urllib.request
-import re
-import pafy
 import yt_dlp
 
-voice_clients = {}
-
-yt_dl_opts = {'default_search': 'auto'}
-ytdl = youtube_dl.YoutubeDL(yt_dl_opts)
-
-ffmpeg_options = {'options': "-vn"}
+FFMPEG_OPTIONS = {
+                'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+                'options': '-vn',
+            }
 
 YTDLP_OPTIONS = {
                 'format': 'bestaudio/best',
@@ -36,34 +29,17 @@ async def play(client, message, url):
 
     channel = message.author.voice.channel
 
-    voice = discord.utils.get(message.guild.voice_channels, name=channel.name)
 
     voice_client = discord.utils.get(client.voice_clients, guild=message.guild)
 
     if voice_client == None:
-        voice_client = await voice.connect()
+        voice_client = await channel.connect()
     else:
         await voice_client.move_to(channel)
 
     with yt_dlp.YoutubeDL(YTDLP_OPTIONS) as ydl:
         info = ydl.extract_info(url, download=False)
-        playUrl = info["url"]
-        print(playUrl)
+        playUrl = info['webpage_url']
 
-    source = discord.FFmpegPCMAudio(playUrl, options=ffmpeg_options)
-    voice.play(source)
-    # search = search.replace(" ", "+")
-
-    # html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + search)
-    # video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-
-        
-    # # await message.channel.send("https://www.youtube.com/watch?v=" + video_ids[0])
-
-    # song = pafy.new(video_ids[0])  # creates a new pafy object
-
-    # audio = song.getbestaudio()  # gets an audio source
-
-    # source = discord.FFmpegPCMAudio(audio.url, **ffmpeg_options)  # converts the youtube audio source into a source discord can use
-
-    # voice_client.play(source)  # play the source
+    source = discord.FFmpegPCMAudio(source=playUrl, **FFMPEG_OPTIONS)
+    voice_client.play(source, after=lambda e: print('Song done'))
